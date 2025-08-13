20ab54b56ecb99ecb08b3bc30217022d20696085
aktin <uaungkyawtin.mti@gmail.com>
2025-08-13 14:16:04 +0700

    initial commit for easy chat

---
+++ src/main/java/com/binary/easychatsystem/EasychatsystemApplication.java
@@ -0,0 +1,13 @@
+package com.binary.easychatsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasychatsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasychatsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binary/easychatsystem/EasychatsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary.easychatsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasychatsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
